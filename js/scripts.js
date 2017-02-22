d7ecfe4d7c5dced3b2c9ec1cb5d0b9fc976234a7
jbailey14 <jbailey14@vcd-ch204-10.eastern.ewu.edu>
2017-02-22 13:45:14 -0800

    Random Images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 // YOUR SCRIPTS GO HERE
+
+// Store 3 images
+var img0 = '../img/200.jpf';
+var img1 = '../img/300.jpf';
+var img2 = '../img/400.jpf';
+
+// Get random number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Display random img
+$('img').attr('src', eval('img' + numRand) );
+
